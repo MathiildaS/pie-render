47a6257feeb6aa926e384ec64539a76a5133fdf5
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-15 15:59:03 +0200

    Create and export StylePie class

---
+++ src/stylepie.js
@@ -0,0 +1,10 @@
+/** 
+ * This class handles the visual configuration of the pie.
+ * 
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ * @version 0.0.1
+ */
+
+export class StylePie {
+
+}
