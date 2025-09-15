42e9d23d2605b9ad03bd6a937960c0c98e4313a1
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-15 15:40:43 +0200

    Create and export ConvertInput class

---
+++ src/convertinput.js
@@ -0,0 +1,10 @@
+/** 
+ * This class converts input values to percentages based on given base-value.
+ * 
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ * @version 0.0.1
+ */
+
+export class ConvertInput {
+
+}
