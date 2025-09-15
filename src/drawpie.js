0d2d727d8cb7e16bede8476d3c4ba99c99f7a354
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-15 15:25:48 +0200

    add constructor in DrawPie class

--- src/drawpie.js
+++ src/drawpie.js
@@ -1,6 +1,12 @@
 /** 
- * This class handles the drawing of the pie.
+ * This class handles the drawing of a pie in canvas.
  * 
  * @author Mathilda Segerlund <ms228qs@student.lnu.se>
  * @version 0.0.1
  */
+
+export class DrawPie {
+    constructor() {
+
+    }
+}
