cfca22cc1792f94053bdabb4c21df3921e8b6747
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-18 11:14:18 +0200

    calculate center coords and radius dynamically to draw a filled circle in createPie method

--- src/drawpie.js
+++ src/drawpie.js
@@ -1,14 +1,41 @@
-/** 
+/**
  * This class handles the drawing of a circle in canvas.
- * 
+ *
  * @author Mathilda Segerlund <ms228qs@student.lnu.se>
  * @version 0.0.1
  */
 
 export class DrawPie {
+  #canvas
   #ctx
 
   constructor(canvasEl) {
+    this.#canvas = canvasEl
     this.#ctx = canvasEl.getContext("2d")
-    }
+  }
+
+  createPie() {
+    this.#ctx.beginPath();
+
+    const centerXCoord = this.#canvas.width / 2
+    const centerYCoord = this.#canvas.height / 2
+
+    const canvasRadie = Math.min(this.#canvas.width, this.#canvas.height) / 2
+    const firstAngle = 0
+    const lastAngle = 2 * Math.PI
+
+    this.#ctx.arc(
+      centerXCoord,
+      centerYCoord,
+      canvasRadie,
+      firstAngle,
+      lastAngle
+    )
+
+    this.#ctx.closePath()
+    this.#ctx.strokeStyle = "#000000"
+    this.#ctx.fillStyle = "#d4d4d4ff"
+    this.#ctx.fill()
+    this.#ctx.stroke()
+  }
 }
