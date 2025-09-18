f5ed32862c3ab7e57f10762a3ebe4b8fc553c0a5
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-18 20:52:07 +0200

    add convertToAngle method that converts the percentage value to an angle in radians and calculateSliceAngles method that calculates the start and end angles based on given percentage value

--- src/convertinput.js
+++ src/convertinput.js
@@ -1,21 +1,58 @@
-/** 
+/**
  * This class converts input values to percentages based on given base-value.
- * 
+ *
  * @author Mathilda Segerlund <ms228qs@student.lnu.se>
  * @version 0.0.1
  */
 
 export class ConvertInput {
-#baseValue
-#inputValue
+  #baseValue
+  #inputValue
+  #startAngle = 0
 
-    constructor(baseValue) {
-        this.#baseValue = baseValue
-    }
+  /**
+   * This constructor initializes the class with a base value.
+   *
+   * @param {number} baseValue - The value that represents 100%.
+   */
+  constructor(baseValue) {
+    this.#baseValue = baseValue
+  }
 
-    convertToPercent(inputValue) {
-        this.#inputValue = inputValue
-        const percentValue = (this.#inputValue / this.#baseValue) * 100
-        return percentValue
-    }
+  /**
+   * This method converts an input value to a percentage based on the base value.
+   *
+   * @param {number} inputValue - The value to be converted to percentage.
+   * @returns {number} percentValue - The value converted to percentage.
+   */
+  convertToPercent(inputValue) {
+    this.#inputValue = inputValue
+    const percentValue = (this.#inputValue / this.#baseValue) * 100
+    return percentValue
+  }
+
+  /**
+   * This method converts a percentage value to an angle in radians.
+   *
+   * @param {number} percentValue - The value in percentage to be converted to angle.
+   * @returns {number} angleValue - The value converted to angle in radians.
+   */
+  convertToAngle(percentValue) {
+    const angleValue = (percentValue / 100) * 2 * Math.PI
+    return angleValue
+  }
+
+  /**
+   * This method calculates the start and end angles for a slice based on the given percentage value.
+   *
+   * @param {number} percentValue - The value in percentage to be converted to slice angles.
+   * @returns { sliceStartAngle: number, endAngle: number } - The start and end angles of the slice.
+   */
+  calculateSliceAngles(percentValue) {
+    const sliceAngle = this.convertToAngle(percentValue)
+    const sliceStartAngle = this.#startAngle
+    const endAngle = sliceStartAngle + sliceAngle
+    this.#startAngle = endAngle
+    return { sliceStartAngle, endAngle }
+  }
 }
