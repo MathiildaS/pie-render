370b214681552bbb73bd414b3ace57c6d2c17440
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-21 21:43:22 +0200

    Add constructor that creates an instance of the PieBoundaries class with default values

--- src/pieboundaries.js
+++ src/pieboundaries.js
@@ -1,10 +1,19 @@
-/** 
- * This class sets the boundaries of the pie.
- * 
+/**
+ * This class manages the warning and danger boundaries for the pie chart.
+ *
  * @author Mathilda Segerlund <ms228qs@student.lnu.se>
  * @version 0.0.1
  */
 
 export class PieBoundaries {
+  #warning
+  #danger
 
+  /**
+   * Creates an instance of the PieBoundaries class with default values.
+   */
+  constructor() {
+    this.#warning = 50
+    this.#danger = 20
+  }
 }
