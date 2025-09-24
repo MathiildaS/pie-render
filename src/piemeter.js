1b4b507e3b5997c7f458367d84ca0b1a15286c5e
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-24 11:34:29 +0200

    Create piemeter.js module and import DrawPie, ConvertInput, PieBoundaries and StylePie classes. Export class PieMeter

---
+++ src/piemeter.js
@@ -0,0 +1,16 @@
+/**
+ * This module is responsible for drawing a pie on a canvas element with methods to create the circular pie, its slices and style it.
+ * It integrates functionalities from DrawPie, ConvertInput, PieBoundaries and StylePie classes.
+ *
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import { DrawPie } from "./drawpie.js"
+import { ConvertInput } from "./convertinput.js"
+import { PieBoundaries } from "./pieboundaries.js"
+import { StylePie } from "./stylepie.js"
+
+export class PieMeter {
+
+}
