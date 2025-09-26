7a885dc1fa944527ba7191c4ce48a57afb300042
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-26 09:42:26 +0200

    Set sliceColour in addSliceColour method

--- src/piemeter.js
+++ src/piemeter.js
@@ -66,7 +66,9 @@ export class PieMeter {
     this.#pieStyle.pieColour = colour
   }
 
-  addSliceColour() {}
+  addSliceColour(colour) {
+    this.#pieStyle.sliceColour = colour
+  }
 
   addPieBoundariesColours() {}
 }
