ff3350b572889ccbf69717dec5135a29de41cebe
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-02 06:55:47 +0200

    Add file description

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+/**
+ * @file The main entry point of the pie-render library
+ *
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ * @version 1.0.0
+ */
 import { PieRender } from "./pierender.js"
 export { PieRender }
